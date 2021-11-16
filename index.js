5f816e0b1fa2eefca1b83196caee15945602beb7
Abdullah Mamun <hello.abdullahmamun1@gmail.com>
2021-11-16 18:07:36 +0600

    get api added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -13,18 +14,26 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqjve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
-
 async function run() {
     try {
         await client.connect();
         const database = client.db("productDB");
         const productCollection = database.collection("products");
 
+        //GET ALL PRODUCTS
         app.get('/products', async (req, res) => {
             const cursor = productCollection.find({})
             const products = await cursor.toArray();
             res.json(products)
+
+        })
+
+        //GET SINGLE PRODUCT
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query)
+            res.json(result)
 
         })
 
